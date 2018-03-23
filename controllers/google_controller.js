5c2b0b9b2745d81349508f819a0753d423d200a2
Ankit Gaur <Ankit.Gaur@ril.com>
2018-03-23 13:23:58 +0530

    - Wrote comments to explain caching for search request.  - Caching should have been done by making use of redis but here we have done by MySQL so as to not use multiple databases.

--- controllers/google_controller.js
+++ controllers/google_controller.js
@@ -29,20 +29,25 @@ var GoogleController = function (cntrls, models) {
 
         search: function (req, res) {
             var userId = req.user.id;
+
+            // Find if such request was made by user ever before
             models.RequestSearch.findOne({where: {userId: userId, lat: req.params.lat, long: req.params.long, radius: req.params.radius, type: req.params.type}}).then(function (savedSearchRequest) {
                 if (savedSearchRequest){
+                    // User have made such request before so just show cached data
                     var alreadyRequestedId = savedSearchRequest.id;
                     models.SavedPlaces.findAll({where: {requestId: alreadyRequestedId}}).then(function (savedPlaces) {
                         res.status(200).send({places: savedPlaces});
                     })
 
                 }else{
+                    // show data real time
                     google.search(req.params.lat, req.params.long, req.params.radius, req.params.type, function handleResponse(error, places) {
                         if (error){
                             res.status(400).send({error: error});
                         }else {
                             res.status(200).send({places: places});
 
+                            // save both request and data for future use
                             var requestToBeSaved = models.RequestSearch.build({
                                 userId: userId,
                                 lat: req.params.lat,
